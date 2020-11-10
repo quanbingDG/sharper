58370a4ac1a336f07afaa82340c17a4cc73d8df5
quanbing <quanbinks@sina.com>
2020-11-10 16:20:26 +0800

    add type_infer

--- pandas_support/pandas_support.py
+++ pandas_support/pandas_support.py
@@ -11,12 +11,24 @@ import pandas as pd
 class PandasSupport:
     @staticmethod
     def check_cols(columns: list, cols: list):
+        '''
+        :param columns: list composed of str
+        :param cols: list composed of str
+        :return: bool， cols'element is all contained with columns
+        '''
         if isinstance(cols, list):
             return all(map(lambda x: x in columns, cols))
         return cols in columns
 
     @staticmethod
     def add_ratio(df_: pd.DataFrame, cols: list, csum=False, check_flag=True):
+        '''
+        :param df_: pandas dataframe
+        :param cols: list composed of str
+        :param csum: whether to add cumsum
+        :param check_flag: whether to check cols legaled
+        :return: df_ with the ratio
+        '''
         if check_flag and PandasSupport.check_cols(df_.columns, cols):
             for col in cols:
                 df_ = PandasSupport.add_ratio(df_, col, csum=csum, check_flag=False)
@@ -30,10 +42,28 @@ class PandasSupport:
 
     @staticmethod
     def add_csum(df_: pd.DataFrame, cols: str):
+        '''
+        :param df_: pandas dataframe
+        :param cols: list composed of str
+        :return: df_ with the cumsum
+        '''
         df_['{0}_cumsum'.format(cols)] = df_[cols].cumsum()
         return df_
+
+    @staticmethod
+    def type_infer(df_: pd.DataFrame, col=None):
+        '''
+        :param df_: pandas dataframe
+        :param col: str
+        :return: dict of dataframe's columns types
+        '''
+        re_ = {}
+        for i in df_.columns:
+            re_[i] = pd.api.types.infer_dtype(df_[i], skipna=True)
+        return re_[col] if col else re_
 
 
 if __name__ == '__main__':
     PS = PandasSupport()
-    print(PS.add_ratio(pd.DataFrame(np.array([1, 2, 3, 4]).reshape(2, 2), columns=['i1', 'i2']), ['i1'],csum=True).columns.__len__())
+    a = pd.DataFrame(np.array([1, 2, 3, 4]).reshape(2, 2), columns=['i1', 'i2'])
+    print(PS.type_infer(a, 'i1'))
