a5ea301bab883cbc535af8cc3f07651da2aeb5a5
quanbing <quanbinks@sina.com>
2020-12-22 16:11:12 +0800

    modify

--- variable_analysis/variable_analysis.py
+++ variable_analysis/variable_analysis.py
@@ -4,10 +4,14 @@
 # @Email : quanbinks@sina.com
 # @File : variable_analysis.py
 
-
+import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import ipywidgets as wg
+from copy import deepcopy
+from IPython.display import display
+from ipywidgets import Layout
 from ..sharper_utils import Utils as ut
 from ..sharper_utils import PlotUtils as put
 
@@ -20,12 +24,16 @@ class VariableAnalysis:
         self._X = data.drop(target, axis=1).copy()
         self._y = data[target].copy()
         self._infer_dict = data.dtypes.to_dict()
+        self._infer_norminal_by_nunique = False
         self._desc = None
         self._desc_slice = None
         self._distribute = None
         self._distribute_with_target = None
         self._stable = None
         self._corr_matrix = None
+        self._include = None
+        self._exclude = None
+        self._cols = data.columns
 
     @property
     def distribute(self):
@@ -38,6 +46,14 @@ class VariableAnalysis:
         if self._corr_matrix is None:
             return self._gen_corr_matrix()
         return self._corr_matrix
+
+    @property
+    def infer_norminal(self):
+        return self._infer_norminal_by_nunique
+
+    @infer_norminal.setter
+    def infer_norminal(self, value: bool):
+        self._infer_norminal_by_nunique = value
 
     @property
     def distribute_with_target(self):
@@ -68,6 +84,37 @@ class VariableAnalysis:
         return self._y
 
     @property
+    def include(self):
+        return self._include
+
+    @include.setter
+    def include(self, value: list):
+        if not isinstance(value, list):
+            raise TypeError("please give me list type")
+        self._include = value
+        self._update_cols()
+
+    @property
+    def exclude(self):
+        return self._exclude
+
+    @exclude.setter
+    def exclude(self, value: list):
+        if not isinstance(value, list):
+            raise TypeError("please give me list type")
+        self._exclude = value
+        self._update_cols()
+
+    @property
+    def cols(self):
+        return self._cols
+
+    def _update_cols(self):
+        _include = self._include if self._include else []
+        _exclude = self._exclude if self._exclude else []
+        self._cols = ut.combin_list(self._data.columns, _include, _exclude)
+
+    @property
     def _data_type(self):
         return self._infer_dict
 
@@ -79,15 +126,30 @@ class VariableAnalysis:
             raise TypeError("please input the value like {'col1': int, 'col2': float}")
 
     @property
-    def infer_data_type(self, infer_norminal=True, threshold=6):
+    def infer_data_type(self, threshold=6):
         modify_cols = {}
-        if infer_norminal:
+        infer_dtype = deepcopy(self._infer_dict)
+
+        if self._infer_norminal_by_nunique:
             for col in self._data.columns:
                 if self._data[col].nunique() < threshold:
                     modify_cols[col] = np.dtype('O')
+            infer_dtype.update(modify_cols)
 
-        self._data_type = modify_cols
-        return self._infer_dict
+        display(wg.Text(
+            value='Following data types have been inferred automatically, '
+                  'if they are correct press enter to continue or type "quit" otherwise.',
+            layout=Layout(width='100%')
+        ), display_id='m1')
+
+        display(pd.DataFrame.from_dict(infer_dtype, orient='index',columns=['infer_type']))
+
+        if input() in ['quit', 'Quit', 'exit', 'EXIT', 'q', 'Q', 'e', 'E', 'QUIT', 'Exit']:
+            sys.exit("Call the 'update_infer_type' method and "
+                     "assign the dtype with dict just like {col_a: float, col_b:str}")
+        else:
+            self._data_type = modify_cols
+            return self._infer_dict
 
     @property
     def modify_dtype(self):
@@ -109,14 +171,14 @@ class VariableAnalysis:
             return "please call the method slice_desc() first !"
         return self._desc_slice
 
-    def gen_desc(self, exclude: list = None, na: list = [np.nan], percentiles=[.01, .1, .5, .75, .9, .99]):
+    def update_infer_type(self, columns: dict):
+        self._data_type = columns
+        return self._infer_dict
+
+    def gen_desc(self, na: list = [np.nan], percentiles=[.01, .1, .5, .75, .9, .99]):
         rows = []
-        cols = self._data.columns.to_list()
 
-        if exclude:
-            cols = ut.minus_list(cols, exclude)
-
-        for name, series in self._data[cols].items():
+        for name, series in self._data[self._cols].items():
             series = series[-series.isin(na)]
             numeric_index = ['mean', 'std', 'min', '1%', '10%', '50%', '75%', '90%', '99%', 'max']
             discrete_index = ['top1', 'top2', 'top3', 'top4', 'top5', 'bottom5', 'bottom4', 'bottom3', 'bottom2',
@@ -151,11 +213,10 @@ class VariableAnalysis:
 
         return self._desc
 
-    def plot_distribute(self, include=[], exclude=[], save_path=None):
+    def plot_distribute(self, save_path=None):
         re = {}
-        plot_cols = ut.combin_list(self._data.columns, include=include, exclude=exclude)
 
-        for col in plot_cols:
+        for col in self._cols:
             try:
                 re[col] = put.plot_distribute_hist(self._data[col], title=col)
             except:
@@ -168,11 +229,10 @@ class VariableAnalysis:
 
         return self._distribute
 
-    def plot_distribute_with_target(self, include=[], exclude=[], save_path=None):
+    def plot_distribute_with_target(self, save_path=None):
         re = {}
-        plot_cols = ut.combin_list(self._data.columns, include=include, exclude=exclude)
 
-        for col in plot_cols:
+        for col in self._cols:
             if ut.is_numeric(self._data[col]):
                 re[col] = put.plot_distribute_with_target(self._data, col, target=self._target)
             else:
@@ -185,16 +245,15 @@ class VariableAnalysis:
 
         return self._distribute_with_target
 
-    def slice_desc(self, by, include=[], exclude=[]):
+    def slice_desc(self, by):
         re = {}
-        cols = ut.combin_list(self._data.columns, include=include, exclude=exclude)
 
         if self._data[by].dtype.kind != 'M':
             self._data[by] = pd.to_datetime(self._data[by])
 
-        continues, norminal = ut.split_numeric_norminal(self._data[cols])
+        continues, norminal = ut.split_numeric_norminal(self._data[self._cols])
 
-        df_ = self._data[cols].set_index(by).resample('M')
+        df_ = self._data[self._cols].set_index(by).resample('M')
 
         _tmp = []
         for col in norminal:
@@ -207,11 +266,9 @@ class VariableAnalysis:
 
         return self._desc_slice
 
-    def plot_stable(self, include=[], exclude=[]):
+    def plot_stable(self):
         if self._desc_slice is None:
             return "please call the method slice_desc() first !"
-
-        # plot_cols = ut.combin_list(self._data.columns, include=include, exclude=exclude)
 
         re = {}
         # continues
@@ -225,9 +282,13 @@ class VariableAnalysis:
         return self._stable
 
     def _gen_corr_matrix(self):
-        self._corr_matrix = {'pearson': self._data.corr(method='pearson'),
-                             'kendall': self._data.corr(method='kendall'),
-                             'spearman': self._data.corr(method='spearman'),
-                             'mutual_info': ut.mutual_info_matrix(self._data)}
+        self._corr_matrix = {'pearson': self._ori_data[self._cols].corr(method='pearson'),
+                             'kendall': self._ori_data[self._cols].corr(method='kendall'),
+                             'spearman': self._ori_data[self._cols].corr(method='spearman'),
+                             'mutual_info': ut.mutual_info_matrix(self._ori_data[self._cols])}
 
         return self._corr_matrix
+
+    def _gen_ks(self):
+        pass
+
