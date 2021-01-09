24c246a560c75664cd8dce8a2eaf0a1a584ce45c
quanbing <quanbinks@sina.com>
2021-01-09 22:30:35 +0800

    cheanup code

--- sharper_utils/plot_utils.py
+++ sharper_utils/plot_utils.py
@@ -113,53 +113,6 @@ class PlotUtils:
         return fig
 
     @staticmethod
-    def plot_distribute_with_target(df_: pd.DataFrame, x: str, target: str, fig_size=(9, 6)):
-        '''
-        :param df_: pandas dataframe
-        :param x: columns name
-        :param target: target columns name
-        :param fig_size: fig_size tuple
-        :return: figure
-        '''
-        fig = plt.figure(figsize=fig_size)
-        bins = len(df_[x].unique()) if len(df_[x].unique()) < 10 else 10
-        df_[str(x) + '_bins'] = pd.cut(df_[x], bins=bins, right=False)
-        fig = df_.groupby([str(x) + '_bins', target]).size().unstack().plot(kind='bar', stacked=False).get_figure()
-        plt.title(x)
-        plt.close('all')
-        return fig
-
-    @staticmethod
-    def plot_distribute_with_target_norminal(df_: pd.DataFrame, x: str, target: str, fig_size=(9, 6)):
-        '''
-        :param df_: pandas dataframe
-        :param x: columns name
-        :param target: target columns name
-        :param fig_size: fig_size tuple
-        :return: figure
-        '''
-        fig = plt.figure(figsize=fig_size)
-        fig = df_.groupby([x, target]).size().unstack().plot(kind='bar', stacked=False).get_figure()
-        plt.title(x)
-        plt.close('all')
-        return fig
-
-    @staticmethod
-    def plots(x: pd.Series, title: str, kind: str, fig_size=(9, 6)):
-        """
-        :param x: pandas series
-        :param title: fig title
-        :param kind: fig type
-        :param fig_size:
-        :return:
-        """
-        fig = plt.figure(figsize=fig_size)
-        fig = x.plot(kind=kind)
-        plt.title(title)
-        plt.close('all')
-        return fig
-
-    @staticmethod
     def save_fig(fig, name, save_path, type_str=None):
         """
         :param type_str:
